7cebdd0d2368d65d9ff0e70046648e327061fc80
Rudru venkatesh <2300031896@kluniversity.in>
2026-03-27 01:53:58 +0530

    fix cors import error

--- carhub-backend/src/main/java/com/carhub/config/CorsConfig.java
+++ carhub-backend/src/main/java/com/carhub/config/CorsConfig.java
@@ -2,9 +2,8 @@ package com.carhub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
